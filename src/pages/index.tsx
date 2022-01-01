9a85eecdadcbee2cb22bcddde4c82409872dee1f
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-01-01 11:59:07 +0900

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <h1 className="text-4xl text-blue-800">Nextjs!!</h1>
+    </div>
+  );
+};
+
+export default Home;
