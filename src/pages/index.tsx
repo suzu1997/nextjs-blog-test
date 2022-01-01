b7c56c716c60227939728debe346c85954267640
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-01-01 20:44:03 +0900

    ブログの一覧ページを作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,108 @@
-import type { NextPage } from 'next';
+import type { GetStaticProps, NextPage } from 'next';
+import { useEffect, useState } from 'react';
+import Cookie from 'universal-cookie';
+import Link from 'next/link';
+import { Layout } from '../components/Layout';
+import { getAllPostsData } from '../lib/fetch';
+import { Post } from '../types/type';
 
-const Home: NextPage = () => {
+const cookie = new Cookie();
+
+type StaticProps = {
+  posts: Array<Post>;
+};
+
+const BlogPage: NextPage<StaticProps> = ({ posts }) => {
+  const [hasToken, setHasToken] = useState<boolean>(false);
+
+  const logout = () => {
+    cookie.remove('access_token');
+    setHasToken(false);
+  };
+
+  const deletePost = async (id: number) => {
+    await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}/delete-blog/${id}`, {
+      method: 'DELETE',
+      // headersのAuthorizationに保持しているaccess_tokenを設定
+      headers: {
+        Authorization: `JWT ${cookie.get('access_token')}`,
+      },
+    }).then((res) => {
+      if (res.status === 401) {
+        alert('JWT token is invalid');
+      }
+    });
+  };
+
+  useEffect(() => {
+    // クッキーにTOKENを保持していれば、ログイン状態とする
+    if (cookie.get('access_token')) {
+      setHasToken(true);
+    }
+  }, []);
+
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <h1 className="text-4xl text-blue-800">Nextjs!!</h1>
-    </div>
+    <Layout title="Blog">
+      <p className="text-4xl mb-10">Blog page</p>
+      <ul>
+        {posts &&
+          posts.map((post) => {
+            return <li key={post.id}>
+              <Link href={`/posts/${post.id}`}>
+                <a className="cursor-pointer border-b border-gray-500 hover:bg-gray-300">
+                  {post.title}
+                </a>
+              </Link>
+              {/* ログインしている場合はゴミ箱アイコン表示 */}
+              {hasToken && (
+                <svg
+                  onClick={() => deletePost(post.id)}
+                  data-testid={`btn-${post.id}`}
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 ml-10 float-right cursor-pointer"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                  />
+                </svg>
+              )}
+            </li>;
+          })}
+      </ul>
+      {hasToken && (
+        <svg
+          onClick={logout}
+          data-testid="logout-icon"
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 mt-10 cursor-pointer"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+          />
+        </svg>
+      )}
+    </Layout>
   );
 };
 
-export default Home;
+export default BlogPage;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await getAllPostsData();
+  return {
+    props: { posts },
+    revalidate: 3,
+  };
+};
