b60fb029de14dd77e54efaad0b13d5ba947b0e50
Suzuhara Chie <mignon.ange.3@gmail.com>
2022-01-01 23:42:32 +0900

    不要なテストファイルを削除

--- __test__/Home.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import Home from '../src/pages/index';
-
-it('Should render hello text', () => {
-  render(<Home />);
-  expect(screen.getByText('Nextjs!!')).toBeInTheDocument();
-});
